c32133568cd08eaf21e5ec4012052c9284e822c9
Dmitry <49916490+Dmitrakas@users.noreply.github.com>
2022-02-22 13:02:12 +0300

    Add files via upload

---
+++ Homework_2 (OOP, class, extension)/Task_2/Task_2_1/DiagonalMatrixHelper.cs
@@ -0,0 +1,28 @@
+﻿public static class DiagonalMatrixHelper
+{
+    public static DiagonalMatrix Add(this DiagonalMatrix a, DiagonalMatrix b)
+    {
+        a ??= new DiagonalMatrix();
+        b ??= new DiagonalMatrix();
+
+        if (a.Size < b.Size)
+        {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        var data = new int[a.Size];
+        for (var i = 0; i < b.Size; i++)
+        {
+            data[i] = a[i, i] + b[i, i];
+        }
+
+        for (var i = b.Size; i < a.Size; i++)
+        {
+            data[i] = a[i, i];
+        }
+
+        return new DiagonalMatrix(data);
+    }
+}
---
+++ Homework_2 (OOP, class, extension)/Task_2/Task_2_2/Training.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Training : Entity
+{
+    private Lesson[] _lessons = new Lesson[0];
+
+    public void Add(Lesson lesson)
+    {
+        if (lesson == null)
+        {
+            return;
+        }
+
+        Array.Resize(ref _lessons, _lessons.Length + 1);
+        _lessons[^1] = lesson;
+    }
+
+    public bool IsPractical()
+    {
+        if (_lessons.Length == 0)
+        {
+            return false;
+        }
+
+        foreach (var lesson in _lessons)
+        {
+            if (!(lesson is Practice))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Training Clone()
+    {
+        var training = new Training {Description = Description};
+        training._lessons = new Lesson[_lessons.Length];
+        for (var i = 0; i < _lessons.Length; i++)
+        {
+            training._lessons[i] = _lessons[i].Clone();
+        }
+
+        return training;
+    }
+}
